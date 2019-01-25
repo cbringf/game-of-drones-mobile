f1fee0deb5e8d3f09ff400e14cb2ffa263dd7739
Celso Bring <cbringf@gmail.com>
2019-01-25 16:40:10 -0600

    adds qr plugin

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { QRScanner, QRScannerStatus } from '@ionic-native/qr-scanner';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+	selector: 'page-home',
+	templateUrl: 'home.html'
 })
 export class HomePage {
+	scanValue: string;
 
-  constructor(public navCtrl: NavController) {
+	constructor(
+		public navCtrl: NavController, private qr: QRScanner
+	) {
+	}
 
-  }
-
+	scan() {
+		this.qr.prepare()
+			.then((status: QRScannerStatus) => {
+				if (status.authorized) {
+					let scanSub = this.qr.scan().subscribe((text: string) => {
+						this.scanValue = text;
+						console.log(text);
+						this.qr.hide();
+						scanSub.unsubscribe();
+					});
+				}
+			});
+	}
 }
